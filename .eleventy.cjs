bc0f2b26583c613bd4567d6b26f4242bd3e5210e
root <root@localhost.localdomain>
2026-03-01 04:39:40 +0000

    Use CJS Eleventy config

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,4 +1,4 @@
-export default function (eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   eleventyConfig.addCollection("patches", (collectionApi) => {
