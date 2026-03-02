9e81d72f904f955711be4109fee994c389730541
root <root@localhost.localdomain>
2026-03-02 20:39:25 +0000

    Fix Eleventy routing and date filter for Netlify

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,22 +1,31 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addNunjucksFilter("date", (value, format = "yyyy") => {
+    const normalized = value === "now" || value == null ? new Date() : (value instanceof Date ? value : new Date(value));
+    if (Number.isNaN(normalized.getTime())) return "";
+    const luxonFormat = format === "%Y" ? "yyyy" : format;
+    return DateTime.fromJSDate(normalized).toFormat(luxonFormat);
+  });
+
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   eleventyConfig.addCollection("specs", (collectionApi) => {
-    const data = collectionApi.getAll()[0].data.specs || [];
+    const data = collectionApi.getAll()[0]?.data?.specs || [];
     return data;
   });
 
   eleventyConfig.addCollection("tagList", (collectionApi) => {
-    const data = collectionApi.getAll()[0].data.specs || [];
+    const data = collectionApi.getAll()[0]?.data?.specs || [];
     const tags = new Set();
-    data.forEach((item) => item.tags.forEach((tag) => tags.add(tag)));
+    data.forEach((item) => (item.tags || []).forEach((tag) => tags.add(tag)));
     return [...tags].sort();
   });
 
   eleventyConfig.addCollection("statusList", (collectionApi) => {
-    const data = collectionApi.getAll()[0].data.specs || [];
+    const data = collectionApi.getAll()[0]?.data?.specs || [];
     const statuses = new Set();
-    data.forEach((item) => statuses.add(item.status));
+    data.forEach((item) => item.status && statuses.add(item.status));
     return [...statuses].sort();
   });
 
@@ -28,4 +37,4 @@ module.exports = function (eleventyConfig) {
       output: "_site",
     },
   };
-}
+};
